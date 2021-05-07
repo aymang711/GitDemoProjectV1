136413b79df7e0ec293baf61e53ab943c27b9fde
user <user@10.0.0.25>
2021-05-07 14:49:52 +0300

    COMMITING TEST 2 BRANCHx

---
+++ src/Test2.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		String str;
+		System.out.println("what you want to send ?:");
+		while(!((str=sc.nextLine()).equals("bye")))
+		{
+			System.out.println("Your Message recived and it is: "+str);
+			System.out.println(" Do you want to send something else ? ,\n if no then send bye;)");
+		}
+			System.out.println("good bye");
+	}
+
+}
